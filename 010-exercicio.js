f2d3feb4bee89297bd015e34769851e4fecefde5
rafamarianoo <rafaelmariano250700@gmail.com>
2026-09-04 21:21:06 -0300

    04-09

---
+++ 010-exercicio.js
@@ -0,0 +1,10 @@
+let hoje = new Date();
+let entrada = require('prompt-sync')();
+let nome = entrada('Digite seu nome: '); 
+let matricula = entrada('Digite sua matrícula: ');
+
+let disciplinas = ['Portugês', 'Matemática', 'História'];
+
+console.log('Data: ');
+console.log(typeof(hoje));
+console.log('ms: ' + typeof());
