0a0822e6def3896829c877bc3ce4a6727a4bf43b
rafamarianoo <rafaelmariano250700@gmail.com>
2026-09-08 20:37:23 -0300

    aula 08-09

--- 010-operadores.js
+++ 010-operadores.js
@@ -11,6 +11,16 @@ let soma = num1 = num2;
 let subtracao = num1 - num2;
 let multiplicacao = num1 * num2;
 let divisao = num1 / num2;
+let inteiroDivisao = parseInt(num1 / num2);
 let restoDivisao = num1 % num2;
 
 console.log(`Soma: ${num1} + ${num2} = ${soma}`);
+console.log(`Subtracao: ${num1} - ${num2} = ${subtracao}`);
+console.log(`Multiplicacao: ${num1} * ${num2} = ${multiplicacao}`);
+console.log(`Divisao: ${num1} ÷ ${num2} = ${divisao.toFixed(2)}`);
+console.log(`Inteiro da Divisao: ${num1} ÷ ${num2} = ${inteiroDivisao}`);
+console.log(`Resto da Divisao: ${num1} ÷ ${num2} sobra: ${restoDivisao}`);
+
+console.log(`Soma: ${num1} + ${num2} = ${Math.round(soma)}`);
+console.log(`Subtracao: ${num1} - ${num2} = ${Math.round(subtracao)}`);
+console.log(`Multiplicacao: ${num1} * ${num2} = ${Math.round(multiplicacao)}`);
---
+++ 011-comparacoes.js
@@ -0,0 +1,6 @@
+let valor1 = 5;
+let valor2 = 3;
+let valor3 = 1;
+let valor4 = 0;
+let valor5 = 'Senha';
+let valor6 = 'Senha';
