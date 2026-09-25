68514de34b5348e0f4910627f0546ba354c7dfce
rafamarianoo <rafaelmariano250700@gmail.com>
2026-09-25 00:12:10 -0300

    Update 013-exercicio.js

--- 013-exercicio.js
+++ 013-exercicio.js
@@ -1,15 +1,15 @@
-let entrada = require('prompt-sync')();
+let entrada = require("prompt-sync")();
 
-let usuario1 = 'Rodrigo';
-let saldo = 1000;
-let usuario2 = 'Maria';
+let usuario1 = "Rodrigo";
+let saldo1 = 1000;
+let usuario2 = "Maria";
 let saldo2 = 500;
-let usuario3 = 'João';
+let usuario3 = "João";
 let saldo3 = 200;
 
-let senha1 = '123456';
-let senha2 = '654321';
-let senha3 = '987654';
+let senha1 = "123456";
+let senha2 = "654321";
+let senha3 = "987654";
 
 let usOK = false;
 let snOK = false;
@@ -17,43 +17,48 @@ let acessoPermitido = false;
 
 let valorDisp = 0;
 
-console.log('Insira seus dados para acessar o sistema!');
+console.log("Insira seus dados para acessar o sistema!");
 
-let loginUsuario = entrada('Nome de usuário: ');
-let loginSenha = entrada('Senha: ');
+let loginUsuario = entrada("Nome de usuário: ");
+let loginSenha = entrada("Senha: ");
 
 if (loginUsuario === usuario1 && loginSenha === senha1) {
-    usOK = true;
-    snOK = true;
-    valorDisp = saldo;
+  usOK = true;
+  snOK = true;
+  valorDisp = saldo1;
 } else if (loginUsuario === usuario2 && loginSenha === senha2) {
-    usOK = true;
-    snOK = true;
-    valorDisp = saldo2;
+  usOK = true;
+  snOK = true;
+  valorDisp = saldo2;
 } else if (loginUsuario === usuario3 && loginSenha === senha3) {
-    usOK = true;
-    snOK = true;
-    valorDisp = saldo3;
+  usOK = true;
+  snOK = true;
+  valorDisp = saldo3;
 }
 
 if (usOK && snOK) {
-    acessoPermitido = true;
+  acessoPermitido = true;
 }
 
 if (acessoPermitido) {
-    console.log('Acesso permitido!');
-    console.log(`Seu saldo é: R$ ${valorDisp}`);
+  console.log("Acesso permitido!");
+  console.log(`Seu saldo é: R$ ${valorDisp}`);
 } else {
-    console.log('Acesso negado!');
+  console.log("Acesso negado!");
 }
-let valorSaque = parseFloat(entrada('Digite o valor que deseja sacar: '));
+let valorSaque = parseFloat(entrada("Digite o valor que deseja sacar: "));
+
 if (valorSaque <= valorDisp) {
-    valorDisp -= valorSaque;
-    console.log(`Saque realizado com sucesso! Seu saldo atual é: R$ ${valorDisp}`);
-}
-else {
-    console.log('Saldo insuficiente para realizar o saque!');
+  valorDisp -= valorSaque;
+  console.log(
+    `Saque realizado com sucesso! Seu saldo atual é: R$ ${ValorDisp}`,
+  );
+
+  console.log("Notas de R$ 50: ${Notas50}");
+  console.log("Valor restante em notas menores: R$ ${restante}");
+} else {
+  console.log("Saldo insuficiente para realizar o saque!");
+  console.lo;
 }
 
-entrada('Pressione enter para finalizar o programa!');
-
+entrada("Pressione enter para finalizar o programa!");
